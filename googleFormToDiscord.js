c5282be735c19896f465b05e87169d4f67c7bb28
Manuel de la Fuente <git@manuelfte.com>
2020-07-11 11:34:29 -0500

    Added link to a hexadecimal to decimal converter.

--- googleFormToDiscord.js
+++ googleFormToDiscord.js
@@ -3,7 +3,7 @@
 /* DO NOT EDIT ABOVE THIS LINE */
 const webhookURL = ''; // Paste your webhook URL within the simple quotes.
 const title = ''; // Specify the title of the embed within the simple quotes. If not provided, it will default to the form's title.
-const color = 7506394; // Color of the embed in decimal.
+const color = 7506394; // Color of the embed in decimal. You can use this hexadecimal to decimal converter: https://www.binaryhexconverter.com/hex-to-decimal-converter
 const timeZone = 'America/Chicago'; // Replace with your timezone code if necessary. Get it from here: https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a
 /* DO NOT EDIT UNDER THIS LINE */
 
