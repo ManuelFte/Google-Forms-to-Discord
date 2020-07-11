28fbced32adb7e0d23db0715e6324ec3f60fca20
Manuel de la Fuente <git@manuelfte.com>
2020-07-11 11:19:01 -0500

    Added a footer that displays the form's submission date.

--- googleFormToDiscord.js
+++ googleFormToDiscord.js
@@ -1,9 +1,10 @@
 /* global UrlFetchApp */
 'use strict';
 /* DO NOT EDIT ABOVE THIS LINE */
-const webhook = ''; // Paste your webhook URL within the simple quotes.
+const webhookURL = ''; // Paste your webhook URL within the simple quotes.
 const title = ''; // Specify the title of the embed within the simple quotes. If not provided, it will default to the form's title.
 const color = 7506394; // Color of the embed in decimal.
+const timeZone = 'America/Chicago'; // Replace with your timezone code if necessary. Get it from here: https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a
 /* DO NOT EDIT UNDER THIS LINE */
 
 const onFormSubmit = (form) => {
@@ -19,16 +20,30 @@ const onFormSubmit = (form) => {
     return field;
   });
 
+  const timestamp = form.response.getTimestamp();
+  const footer = timestamp.toLocaleDateString('en-US', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone,
+    timeZoneName: 'short'
+  });
+
   const payload = {
     embeds: [{
       type: 'rich',
       color,
       title: title || form.source.getTitle(),
-      fields
+      fields,
+      footer: { text: footer }
     }]
   };
 
-  UrlFetchApp.fetch(webhook, {
+  UrlFetchApp.fetch(webhookURL, {
     method: 'post',
     payload: JSON.stringify(payload),
     contentType: 'application/json'
